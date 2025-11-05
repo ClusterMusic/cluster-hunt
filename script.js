03cc63ce1f0f22cc19b5567a5ec7a880f8b53468
Blvze123 <rishabh1dey@gmail.com>
2025-11-04 19:54:23 -0500

    changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const TOTAL_SPOTS = 5;
-const API_CALL = "https://script.google.com/macros/s/AKfycbyDucFaEAxK-h0YnKzadmG67U96kc3frASGRBG1LmLEUMoAaaPJfhaZMsAy4PHDDhUb/exec";
+const API_CALL = "https://script.google.com/macros/s/AKfycbyiDmc7XBl8cS4B6t0SqL-w2FqaUoci6VAdWaxmJxAGlCQ-X3v5i1fESpWVapcsyQYz/exec";
 
 function markVisited(spotId) {
   let progress = JSON.parse(localStorage.getItem("visitedSpots") || "{}");
